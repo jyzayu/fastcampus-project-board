17ce0e51c87fb94144f4cd7f72772d88b3bd7877
JY CHOI <moosou30@naver.com>
2022-10-29 10:28:33 +0900

    프로젝트를 git 폴더로 옮김,domain 작성
    프로젝트를 git 폴더로 옮김, domain(Article, AuditingFields, UserAccount)작성

---
+++ project-board/src/main/java/com/fastcampus/projectboard/FastcapmusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcapmusProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcapmusProjectBoardApplication.class, args);
+	}
+
+}
