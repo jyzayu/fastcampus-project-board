36e8c286593694ae58cf75d3d4fabccaee6bac9e
JY CHOI <moosou30@naver.com>
2022-12-17 15:20:23 +0900

    gitignore

--- src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
@@ -24,7 +24,7 @@ public record UserAccountDto (
     public UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo) {
         return new UserAccountDto(userId,userPassword,email,nickname, memo, null, null, null, null);
     }
-    
+
 
 
     public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
