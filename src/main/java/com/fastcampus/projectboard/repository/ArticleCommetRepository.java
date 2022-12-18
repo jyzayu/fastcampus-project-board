031b07c8875d804a2f3191a17b6223409e998676
JY CHOI <moosou30@naver.com>
2022-12-18 21:56:25 +0900

    #1 Article, Comment Repository 구현, RepoTest 작성
    CommetRepo -> CommentRepo 오타 수정 및 부분 검색 구현, ArticleRepo hashTag제외 구현,
    UserAccount는 부분 검색할 일 없음. 
    RepoTest hashTag제외한 select, insert 테스트 작성

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fastcampus.projectboard.repository;
-
-import com.fastcampus.projectboard.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ArticleCommetRepository extends JpaRepository<ArticleComment, Long> {
-}
--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -1,70 +1,71 @@
-//package com.fastcampus.projectboard.repository;
-//
-//import com.fastcampus.projectboard.config.JpaConfig;
-//import com.fastcampus.projectboard.domain.Article;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//@DisplayName("JPA 연결 테스트")
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.config.JpaConfig;
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.UserAccount;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("JPA 연결 테스트")
 //@Import(JpaRepositoryTest.TestJpaConfig.class)
-//@DataJpaTest
-//class JpaRepositoryTest {
-//
-//    private final ArticleRepository articleRepository;
-//    private final ArticleCommentRepository articleCommentRepository;
-//    private final UserAccountRepository userAccountRepository;
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 //    private final HashtagRepository hashtagRepository;
-//
-//    JpaRepositoryTest(
-//            @Autowired ArticleRepository articleRepository,
-//            @Autowired ArticleCommentRepository articleCommentRepository,
-//            @Autowired UserAccountRepository userAccountRepository,
+
+    JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository,
+            @Autowired UserAccountRepository userAccountRepository
 //            @Autowired HashtagRepository hashtagRepository
-//    ) {
-//        this.articleRepository = articleRepository;
-//        this.articleCommentRepository = articleCommentRepository;
-//        this.userAccountRepository = userAccountRepository;
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
 //        this.hashtagRepository = hashtagRepository;
-//    }
-//
-//    @DisplayName("select 테스트")
-//    @Test
-//    void givenTestData_whenSelecting_thenWorksFine() {
-//        // Given
-//
-//        // When
-//        List<Article> articles = articleRepository.findAll();
-//
-//        // Then
-//        assertThat(articles)
-//                .isNotNull()
-//                .hasSize(123); // classpath:resources/data.sql 참조
-//    }
-//
-//    @DisplayName("insert 테스트")
-//    @Test
-//    void givenTestData_whenInserting_thenWorksFine() {
-//        // Given
-//        long previousCount = articleRepository.count();
-//        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newUno", "pw", null, null, null));
-//        Article article = Article.of(userAccount, "new article", "new content");
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123); // classpath:resources/data.sql 참조
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // Given
+        long previousCount = articleRepository.count();
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newUno", "pw", null, null, null));
+        Article article = Article.of(userAccount, "new article", "new content");
 //        article.addHashtags(Set.of(Hashtag.of("spring")));
-//
-//        // When
-//        articleRepository.save(article);
-//
-//        // Then
-//        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
-//    }
-//
+
+        // When
+        articleRepository.save(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+}
 //    @DisplayName("update 테스트")
 //    @Test
 //    void givenTestData_whenUpdating_thenWorksFine() {
